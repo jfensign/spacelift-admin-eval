afe983d81a1f0b7fef828884bcd0506948d55315
Joseph Ensign <jfensign@users.noreply.github.com>
2021-07-01 15:56:58 -0400

    Adds environment Context

--- tml/cloud-environment/outputs.tf
+++ tml/cloud-environment/outputs.tf
@@ -1,10 +1,21 @@
-
-output "name" {
-  value = var.environment_name
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
 }
 
-output "labels" {
-  value = merge(var.environment_default_labels, {
-    environment_name = var.environment_name
-  })
+provider "spacelift" {}
+
+resource "spacelift_context" "environment_context" {
+  description = "Platform environment context for ${var.environment_name}"
+  name        = "platform-environment-${var.environment_name}"
+}
+
+resource "spacelift_environment_variable" "environment_name" {
+  context_id = spacelift_context.environment_context.id
+  name       = "ENVIRONMENT"
+  value      = var.environment_name
+  write_only = false
 }
