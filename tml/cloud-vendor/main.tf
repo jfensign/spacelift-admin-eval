844ab62b033d034941183bc7f66e8a44a5e26e45
Joseph Ensign <jfensign@users.noreply.github.com>
2021-07-01 15:32:54 -0400

    Update and rename outputs.tf to main.tf

---
+++ tml/cloud-vendor/main.tf
@@ -0,0 +1,19 @@
+
+resource "spacelift_context" "vendor_context" {
+  description = "Platform vendor context for ${var.vendor_name}"
+  name        = "platform-vendor-${var.vendor_name}"
+}
+
+resource "spacelift_environment_variable" "vendor_name" {
+  context_id = splacelift_context.vendor_context.id
+  name       = "VENDOR_NAME"
+  value      = var.vendor_name
+  write_only = false
+}
+
+resource "spacelift_environment_variable" "vendor_pwho" {
+  context_id = splacelift_context.vendor_context.id
+  name       = "VENDOR_PWHO"
+  value      = var.vendor_pwho
+  write_only = false
+}
