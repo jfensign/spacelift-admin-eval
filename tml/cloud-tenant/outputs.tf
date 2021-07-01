090ad5a8079d3ec0ff831f10cc35204441247b66
Joseph Ensign <jfensign@users.noreply.github.com>
2021-07-01 16:12:06 -0400

    Update outputs.tf

--- tml/cloud-tenant/outputs.tf
+++ tml/cloud-tenant/outputs.tf
@@ -7,9 +7,31 @@ output "group_id" {
   value = var.tenant_group_id
 }
 
-output "labels" {
-  value = merge(var.tenant_default_labels, {
-    tenant_name = var.tenant_name
-    tenant_group_id = var.tenant_group_id
-  })
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+    }
+  }
+}
+
+provider "spacelift" {}
+
+resource "spacelift_context" "tenant_context" {
+  description = "Platform tenant context for ${var.tenant_name}"
+  name        = "platform-tenant-${var.tenant_name}"
+}
+
+resource "spacelift_environment_variable" "tenant_name" {
+  context_id = spacelift_context.tenant_context.id
+  name       = "TENANT_NAME"
+  value      = var.tenant_name
+  write_only = false
+}
+
+resource "spacelift_environment_variable" "tenant_group_id" {
+  context_id = spacelift_context.tenant_context.id
+  name       = "TENANT_GROUP_ID"
+  value      = var.tenant_group_id
+  write_only = false
 }
